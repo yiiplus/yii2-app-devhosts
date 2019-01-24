19924a00a48186dd3b225415687a91b9510c3806
phperwangjunchao <1556595721@qq.com>
2019-01-24 22:21:16 +0800

    fix:i18转汉字

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -140,6 +140,6 @@ class DefaultController extends Controller
             return $model;
         }
 
-        throw new NotFoundHttpException(Yii::t('yiiplus/kvstore','请求的页面不存在!'));
+        throw new NotFoundHttpException(Yii::t('yiiplus/kvstore','请求的页面不存在'));
     }
 }
